cbd4450b3981819f1e67c2aa0a42228e9007b297
KAYNALEDI <nokwazipalesa@gmail.com>
2024-07-09 16:55:42 -0600

    json

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+
 const express = require('express');
-const app = express();
 
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
-app.use('/', require(',/routes'))
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
 
-app.listen(port, () => {console.log('running on port ${port}')})
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
